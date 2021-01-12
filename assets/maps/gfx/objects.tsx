14f18a87cd4e00824a178fec4e6822e820949b87
Dyllan Kobal <dddk500@gmail.com>
2021-01-11 22:43:25 -0500

    added all the assets

---
+++ assets/maps/gfx/objects.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="objects" tilewidth="16" tileheight="16" tilecount="660" columns="33">
+ <image source="objects.png" width="528" height="320"/>
+ <tile id="103">
+  <animation>
+   <frame tileid="103" duration="100"/>
+   <frame tileid="104" duration="100"/>
+   <frame tileid="105" duration="100"/>
+   <frame tileid="106" duration="100"/>
+   <frame tileid="107" duration="100"/>
+   <frame tileid="108" duration="100"/>
+   <frame tileid="109" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="614">
+  <animation>
+   <frame tileid="132" duration="100"/>
+   <frame tileid="133" duration="100"/>
+   <frame tileid="134" duration="100"/>
+   <frame tileid="135" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
